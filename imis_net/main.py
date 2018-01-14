1ec1c8c7a3d811afffa51d1caa36481b28f8a797
Przemysław Pietras <przemyslawp94@gmail.com>
2018-01-14 14:36:13 +0100

    normalizacja danych wejściowych

--- imis_net/main.py
+++ imis_net/main.py
@@ -12,7 +12,7 @@ data = []
 labels = []
 width = 800
 height = 600
-max_size = 150
+max_size = 5
 directory_path = 'D:/samochody'
 
 for i in range(1, max_size + 1):
@@ -30,6 +30,11 @@ print('Loaded', len(dataframe), 'labels.                               ')
 x_train = np.reshape(np.array(data) / 255.0, (max_size, height, width, 3))
 # y_train = keras.utils.to_categorical(np.reshape(np.array(labels), (max_size, 1)), num_classes=2)
 y_train = dataframe.values[:max_size, 1:]
+y_train[:, 0] /= width
+y_train[:, 1] /= height
+y_train[:, 2] /= width
+y_train[:, 3] /= height
+
 print('x_train shape:', x_train.shape)
 print('y_train shape:', y_train.shape)
 
@@ -46,7 +51,7 @@ print(model.summary())
 
 print('LEARNING'.center(65, '='))
 
-model.fit(x=x_train, y=y_train, epochs=10, batch_size=4)
+model.fit(x=x_train, y=y_train, epochs=10, batch_size=4, validation_split=0.3)
 
 # loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
 
@@ -63,10 +68,10 @@ main_panel.add(canvas, width=width, height=height)
 img = Image.open(directory_path + '/IMG' + str(max_size + 1) + '.jpg').resize((width, height), Image.ANTIALIAS)
 img = ImageTk.PhotoImage(img)
 canvas.create_image(0, 0, anchor=tk.NW, image=img)
-canvas.create_rectangle(y_pred[0][0],
-                        y_pred[0][1],
-                        y_pred[0][0] + y_pred[0][2],
-                        y_pred[0][1] + y_pred[0][3],
+canvas.create_rectangle(y_pred[0][0] * width,
+                        y_pred[0][1] * height,
+                        (y_pred[0][0] + y_pred[0][2]) * width,
+                        (y_pred[0][1] + y_pred[0][3]) * height,
                         outline='green',
                         width=2)
 root.mainloop()
